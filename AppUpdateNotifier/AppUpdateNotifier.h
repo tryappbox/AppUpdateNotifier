74d5ba426c1aba9de8c4cbff8304805bc4c8307b
iTamilan <Saranya@Tamilarasus-MacBook-Pro.local>
2017-01-03 02:04:21 +0530

    Initial Commit

---
+++ AppUpdateNotifier/AppUpdateNotifier.h
@@ -0,0 +1,19 @@
+//
+//  AppUpdateNotifier.h
+//  AppUpdateNotifier
+//
+//  Created by iTamilan on 03/01/2017.
+//  Copyright © 2017 iTamilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppUpdateNotifier.
+FOUNDATION_EXPORT double AppUpdateNotifierVersionNumber;
+
+//! Project version string for AppUpdateNotifier.
+FOUNDATION_EXPORT const unsigned char AppUpdateNotifierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppUpdateNotifier/PublicHeader.h>
+
+#import <AppNewVersionNotifier.h>
